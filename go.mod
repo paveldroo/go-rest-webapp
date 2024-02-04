b24c8aff5783b784e998417087070d6711b5d66e
Pavel Druzhinin <paveldroo@gmail.com>
2024-02-04 19:51:26 +0300

    Add User model and json marshaling

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
-module gp-rest-webapp
+module go-rest-webapp
 
 go 1.21
 
-require github.com/julienschmidt/httprouter v1.3.0 // indirect
+require github.com/julienschmidt/httprouter v1.3.0
--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/julienschmidt/httprouter"
+	"go-rest-webapp/models"
 	"log"
 	"net/http"
 )
@@ -10,9 +12,36 @@ import (
 func main() {
 	r := httprouter.New()
 	r.GET("/", index)
+	r.GET("/user/:id", getUser)
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
 
 func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "WELCOME!\n")
+	s := `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+			<meta charset="UTF-8">
+			<title>Index</title>
+			</head>
+			<body>
+			<a href="/user/9872309847">GO TO: http://localhost:8000/user/9872309847</a>
+			</body>
+			</html>`
+	w.Header().Set("Content-Type", "text/html; charset=utf8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(s))
+}
+
+func getUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	u := models.User{
+		Name:   "Jonny",
+		Gender: "male",
+		Age:    28,
+		Id:     p.ByName("id"),
+	}
+
+	uj, _ := json.Marshal(u)
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, "%s\n", uj)
+
 }
