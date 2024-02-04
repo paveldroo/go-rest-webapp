f1c0f67f40b82c8a94f101d934a230d3f35b9801
Pavel Druzhinin <paveldroo@gmail.com>
2024-02-04 23:28:14 +0300

    Use mongo db for CRUDing users via webapp endpoints

--- controllers/user.go
+++ controllers/user.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"go-rest-webapp/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"net/http"
 )
@@ -18,12 +20,15 @@ func NewUserController(c *mongo.Client) *UserController {
 }
 
 func (uc UserController) GetUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	u := models.User{
-		Name:   "Jonny",
-		Gender: "male",
-		Age:    28,
-		Id:     p.ByName("id"),
+	uId, err := primitive.ObjectIDFromHex(p.ByName("id"))
+	if err != nil {
+		fmt.Println(err)
 	}
+	res := uc.client.Database("go-mongo").Collection("users").FindOne(r.Context(), bson.M{"_id": uId})
+
+	u := models.User{}
+
+	res.Decode(&u)
 
 	uj, _ := json.Marshal(u)
 	w.Header().Set("Content-Type", "application/json")
@@ -37,15 +42,29 @@ func (uc UserController) CreateUser(w http.ResponseWriter, r *http.Request, _ ht
 
 	_ = json.NewDecoder(r.Body).Decode(&u)
 
-	u.Id = "007"
-	uj, _ := json.Marshal(u)
+	u.Id = primitive.NewObjectID()
+	//ub, _ := bson.Marshal(u)
+
+	res, err := uc.client.Database("go-mongo").Collection("users").InsertOne(r.Context(), bson.M{})
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
-	fmt.Fprintf(w, "%s\n", uj)
+	fmt.Fprintf(w, "User %s has been added!\n", res.InsertedID)
 }
 
 func (uc UserController) DeleteUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	// We will delete user here
+	uId, err := primitive.ObjectIDFromHex(p.ByName("id"))
+	if err != nil {
+		fmt.Println(err)
+	}
+	res, err := uc.client.Database("go-mongo").Collection("users").DeleteOne(r.Context(), bson.M{"_id": uId})
+	if err != nil {
+		http.Error(w, "No user found", http.StatusNotFound)
+		return
+	}
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "User was deleted!")
+	fmt.Fprintf(w, string(res.DeletedCount), "users were deleted!")
 }
