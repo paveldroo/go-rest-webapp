cbf690f3fc28ea97e0f800ea5f653f3d270e7c46
Pavel Druzhinin <paveldroo@gmail.com>
2024-02-04 19:34:34 +0300

    Add http router and index func

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+)
 
+func main() {
+	r := httprouter.New()
+	r.GET("/", index)
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
+
+func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "WELCOME!\n")
 }
